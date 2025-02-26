e076cde61c5f960f28511bf445ffa2075f8e2a29
Jarred Villegas <jarredvillegas2@gmail.com>
2025-02-26 19:48:49 +0800

    jarred commit

--- components/ClientScreens/ProfileScreen.tsx
+++ components/ClientScreens/ProfileScreen.tsx
@@ -1,10 +1,27 @@
 import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { View, Text, TouchableOpacity, StyleSheet } from 'react-native';
 
 const ProfileScreen = () => {
   return (
     <View style={styles.container}>
-      <Text style={styles.text}>👤 Profile Screen</Text>
+      <View style={styles.profilecontainer}>
+      {/* <Text style={styles.title}>👤 Profile Screen</Text> */}
+      </View>
+      <TouchableOpacity style={styles.item}>
+        <Text style={styles.text}>📅 My Event</Text>
+      </TouchableOpacity>
+      
+      <TouchableOpacity style={styles.item}>
+        <Text style={styles.text}>⭐ Favourite Supplier</Text>
+      </TouchableOpacity>
+      
+      <TouchableOpacity style={styles.item}>
+        <Text style={styles.text}>💳 Payment and Booking</Text>
+      </TouchableOpacity>
+      
+      <TouchableOpacity style={styles.item}>
+        <Text style={styles.text}>🚪 Logout</Text>
+      </TouchableOpacity>
     </View>
   );
 };
@@ -12,12 +29,44 @@ const ProfileScreen = () => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
+    padding: 20,
+    backgroundColor: '#f5f5f5',
+  },
+  profilecontainer:{
+    backgroundColor: '#5392DD',
+    width: '115%',
+    height:'45%',
     alignItems: 'center',
+    alignSelf: 'center',
+    borderBottomRightRadius: 55,
+    borderBottomLeftRadius: 55,
+    bottom: '5%',
+    shadowColor: '#000',
+    shadowOffset: { width: 0, height: 2 },
+    shadowOpacity: 0.1,
+    shadowRadius: 5,
+  },
+  title: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    textAlign: 'center',
+    marginBottom: 20,
+  },
+  item: {
+    backgroundColor: '#fff',
+    padding: 15,
+    borderRadius: 10,
+    marginBottom: 15,
+    height: 80,
+    alignItems: 'center',
+    shadowColor: '#000',
+    shadowOffset: { width: 0, height: 2 },
+    shadowOpacity: 0.1,
+    shadowRadius: 5,
   },
   text: {
-    fontSize: 20,
-    fontWeight: 'bold',
+    fontSize: 18,
+    fontWeight: '500',
   },
 });
 
